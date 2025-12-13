446d68874ce5065eadb118ac461210296b1e0cb2
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2025-12-13 11:40:19 +0530

    making new line iterator

--- src/04_iterator_lines.js
+++ src/04_iterator_lines.js
@@ -1,10 +1,12 @@
 // - Iterate over lines of text
 //   "this\nis\ngood" => ['this','is','good']
 
-export const lineIterator = () => {
-  return '';
+export function* lineIterator(text ) {
+  let i = 0
+  while(i < text.length) {
+    const indexOfLine = text.indexOf('\n', i) === -1 ? text.length : text.indexOf('\n', i);
+    yield text.slice(i, indexOfLine);
+    i = indexOfLine + 1;
+  }
 }
 
-
-// const text =  "this\nis\ngood";
-// lineIterator(text)
--- test/iterators_test.js
+++ test/iterators_test.js
@@ -1,7 +1,27 @@
 import { assertEquals } from '@std/assert';
 import { lineIterator } from "../src/04_iterator_lines.js";
-// import {} from 
 
 Deno.test('simple test' , () => {
-  assertEquals(lineIterator(), '')
+  const text =  "this\nis\ngood";
+  const result = lineIterator(text)
+  assertEquals([...result], ['this', 'is', 'good']);
+}) 
+
+Deno.test('hi\nhello\nhow are you' , () => {
+  const text =  "hi\nhello\nhow are you";
+  const result = lineIterator(text)
+  assertEquals([...result], [ "hi", "hello", "how are you" ]);
+}) 
+
+Deno.test('only hi ,there is no newline' , () => {
+  const text =  "hi";
+  const result = lineIterator(text)
+  assertEquals(result.next().value, "hi" );
+}) 
+
+Deno.test('undefined value' , () => {
+  const text =  "hi";
+  const result = lineIterator(text);
+  result.next();
+  assertEquals(result.next().value, undefined );
 }) 
